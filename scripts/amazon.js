325d3c02f857035600579101fbf0d1bae282c158
Sprout <apacibleeoghan@gmail.com>
2025-08-16 14:46:52 +0800

    Created the deliveryOption data as well as generating the deliveryOption html. It is still not interactive though

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,6 +1,6 @@
 import { addToCart } from "../data/cart.js";
 import { products } from "../data/products.js";
-import { formatCurrency } from "./utils/money.js";
+import  formatCurrency  from "./utils/money.js";
 import { updateCartQuantity } from "./utils/updateCartQuantity.js";
 
 /* Never ever export a function with a document.querySelector element to a js file
