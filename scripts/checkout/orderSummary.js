a4b6a58b52f98952500c9dc38471cb8e611b710f
Sprout <apacibleeoghan@gmail.com>
2025-08-24 17:48:18 +0800

    Saved the data and generated the html for the paymentSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,8 +1,8 @@
 import { cart, changeCartQuantity, removeFromCart, updateDeliveryOption } from '../../data/cart.js'
-import { products } from '../../data/products.js';
+import { products, getProduct } from '../../data/products.js';
 import { updateCartQuantity } from '../utils/updateCartQuantity.js';
 import { formatCurrency } from '../utils/money.js';
-import { deliveryOptions } from '../../data/deliveryOptions.js';
+import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js';
 import { formatTime } from '../utils/formatTime.js';
 
 
@@ -26,23 +26,11 @@ export function renderOrderSummary () {
   cart.forEach((cartItem) => {
   const productId = cartItem.productId
 
-  let matchingProduct;
-
-  products.forEach((product) => {
-    if(product.id === productId) {
-      matchingProduct = product;
-    }
-  });
+  const matchingProduct = getProduct(productId);
 
   const deliveryOptionId = cartItem.deliveryOptionId;
 
-  let deliveryOption;
-
-  deliveryOptions.forEach((option) => {
-    if(option.id === deliveryOptionId){
-      deliveryOption = option;
-    }
-  });
+  const deliveryOption = getDeliveryOption(deliveryOptionId)
 
   const dateString = formatTime(deliveryOption);
 
