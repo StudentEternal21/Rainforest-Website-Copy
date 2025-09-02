51e0cd90b8d2a43a7e6af04807d6b347e27ea971
Sprout <apacibleeoghan@gmail.com>
2025-09-02 17:42:58 +0800

    Added money tests in the jasmine framework

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,23 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
+// the describe function creates the test suite
+describe('test suite: formatCurrency', () => {
+  //The it function creates the test/edge case name
+
+  it('converts cents into dollars', () => {
+    /*the expect function allows us to compare two values
+    It has a toEqual object that allows the comparison itself to occur
+    */
+    expect(formatCurrency(2095)).toEqual('20.95');
+  });
+
+  it('works with zero 0', () => {
+    expect(formatCurrency(0)).toEqual('0.00');
+  });
+
+  it('rounds up to the nearest cent', () => {
+    expect(formatCurrency(2000.5)).toEqual('20.01');
+  });
+
+
+});
