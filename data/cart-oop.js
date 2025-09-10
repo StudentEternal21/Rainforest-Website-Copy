cb0e3e849a0fac5b7158b96242afeb148b854760
StudentEternal21 <apacibleenegueeoghan@gmail.com>
2025-09-10 10:07:31 +0800

    Created an oop version of a cart

--- data/cart-oop.js
+++ data/cart-oop.js
@@ -1,10 +1,9 @@
-import { loadFromStorage } from "./cart";
-
-const cart = {
+function Cart(localStorageKey) {
+  const cart = {
   cartItems: undefined,
 
   loadFromStorage() {
-     this.cartItems = JSON.parse(localStorage.getItem('cart-oop')) ||[{
+     this.cartItems = JSON.parse(localStorage.getItem(localStorageKey)) ||[{
      productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
      quantity: 2, 
      deliveryOptionId: '1'
@@ -17,7 +16,7 @@ const cart = {
     },
 
     saveToStorage () {
-      localStorage.setItem('cart-oop', JSON.stringify(this.cartItems))
+      localStorage.setItem(localStorageKey, JSON.stringify(this.cartItems))
     },
 
     addToCart(productId) {
@@ -103,7 +102,20 @@ const cart = {
           return; 
         }
       });
-    
-    
     }
+};
+return cart;
 }
+
+const cart = Cart('cart-oop');
+const businessCart = Cart('cart-business');
+
+
+cart.loadFromStorage();
+businessCart.loadFromStorage();
+
+console.log(cart);
+console.log(businessCart);
+
+
+
