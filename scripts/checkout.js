0edfca41cbb4e2e3d1ac055cf5d26b9dfc886eb8
StudentEternal21 <apacibleenegueeoghan@gmail.com>
2025-09-27 11:20:54 +0800

    Added an explanation to checkout and convert from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,7 +3,32 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 //import "../data/backend-practice.js";
+/*The async keyword basically wraps the function inside the argument of a promise function
+which returns a promise so you can use then after calling the async function. Or most importantly
+use the await feature which is basically the promise.all method which waits for all the await function
+starting from the first await to the last. since it has an await method then you can just put the code
+that is supposed to be for the then below all the await functions removing the need for the then. */
+async function loadPage() {
 
+
+  await loadProductsFetch();
+
+  await new Promise((resolve) => {
+    loadCart(() => {
+    resolve();
+    });
+  });
+
+  renderOrderSummary();
+  renderPaymentSummary();
+
+}
+
+loadPage()
+/*The Promise class has another method called all which has an array as an argument
+with the values being promises. It works the same way with then that after all the promises resolve
+then it starts the then code block inside. */
+/*
 Promise.all([
   loadProductsFetch(),
    
@@ -17,6 +42,12 @@ Promise.all([
   renderOrderSummary();
   renderPaymentSummary();
 });
+
+*/
+
+/*Regular promises with the 'then' method. since  Promise is a built-in class we use new to 
+instantiate a new object for a promise. at the end of a promise we append a method for then. the then 
+method waits for the promise to resolve then start the code inside the then in a different thread.*/
 /*
 new Promise((resolve) => {
 
@@ -39,10 +70,14 @@ new Promise((resolve) => {
     renderPaymentSummary(); 
 });
 */
-// loadProducts(() => {
-//   loadCart(() => {
-//     renderOrderSummary();
-//     renderPaymentSummary();
-//   });
+// This is for the Callback function when creating asynchronous code. (Callback hell when more than two)
 
-// });
+/*
+loadProducts(() => {
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+
+});
+*/
